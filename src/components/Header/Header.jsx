0c32c8aa5e41744ef779705f1b53da3986f86187
Elchin <112569510+yungalesgerov@users.noreply.github.com>
2024-02-22 22:19:06 +0300

    prepare for eject

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.css';
+
+const Header = () => {
+    return (
+        <div className={styles.Header}>
+            1
+        </div>
+    );
+};
+
+export default Header;
